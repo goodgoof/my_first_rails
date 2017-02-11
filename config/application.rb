ae4ea6bf989a27ed28ab5a39447f40e2ed09e68f
Anjali Sharma <anjalidsharma8@gmail.com>
2017-02-11 15:23:33 -0500

    Made changes in config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
-config.assets.initialize_on_precompile = false #  this line 14 added by Anjali after seeing on
+#config.assets.initialize_on_precompile = false #  this line 14 added by Anjali after seeing on
 #stackoverflow.
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
