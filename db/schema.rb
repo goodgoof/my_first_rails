8731938318d64ac4365cea3b0f1c6234ca803192
Anjali Sharma <anjalidsharma8@gmail.com>
2017-03-03 21:07:01 -0500

    added color and price field to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170214170953) do
+ActiveRecord::Schema.define(version: 20170302190720) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,9 @@ ActiveRecord::Schema.define(version: 20170214170953) do
     t.string   "imgage_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "color"
+    t.string   "string"
+    t.decimal  "price"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+Product.create(:name => "seed-generated product", :description => "I also created this product without using the HTML form!",imgage_url:"http://media.istockphoto.com/photos/kitten-on-a-white-background-picture-id511458158?k=6&m=511458158&s=170667a&w=0&h=YS27-I0nj5x89iDRUZ2YxE77hEghPb0HNCWwPCrMmn8=")
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
