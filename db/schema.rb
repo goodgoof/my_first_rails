b80ae363bbf7817b60156d6ed5eeee3daa7cb2bf
Anjali Sharma <anjalidsharma8@gmail.com>
2017-03-07 13:07:28 -0500

    Resolved conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 20170302190720) do
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
-    t.string   "imgage_url"
+    t.string   "image_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "color"
