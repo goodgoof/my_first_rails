8b0cb99781fa91a86242fb765f7669820504522b
goodgoof <anjalidsharma8@gmail.com>
2017-04-07 12:51:36 -0400

    static pages controller changed

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -17,7 +17,7 @@ class StaticPagesController < ApplicationController
   @name = params[:name]
   @email = params[:email]
   @message = params[:message]
-  UsermailerMailer.contact_form(@email, @name, @message).deliver_now
+  UserMailer.contact_form(@email, @name, @message).deliver_now
   end
 
   
