bd4005ca58bf1577f0a81dd5400275e672bfbb39
goodgoof <anjalidsharma8@gmail.com>
2017-04-10 09:43:05 -0400

    Created new branch Authentication and added User scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170310180731) do
+ActiveRecord::Schema.define(version: 20170410021632) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 20170310180731) do
     t.string   "string"
     t.decimal  "price"
     t.string   "image_url"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
