7fd8fb22edb2be4ec038389a0f62986d964e687d
goodgoof <anjalidsharma8@gmail.com>
2017-04-25 13:21:35 -0400

    5.8 rating system

---
+++ app/assets/javascripts/site.js
@@ -0,0 +1,10 @@
+$(document).on('turbolinks:load', function(){
+    $(".alert, .notice").delay(4000).fadeOut(1000);
+    $(".rating").raty( { path: "/assets", scoreName: "comment[rating]" });
+    $(".rated").raty( { path: "/assets", 
+    readOnly: true,
+    score: function() {
+      return $(this).attr("data-score");
+    }
+  });
+});
