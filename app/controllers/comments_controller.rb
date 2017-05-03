0a08640f11c2050d66343d192d38262f550880b9
goodgoof <anjalidsharma8@gmail.com>
2017-05-03 13:38:30 -0400

    comment not getting saved

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,7 @@ class CommentsController < ApplicationController
 	  @comment = @product.comments.new(comment_params)
 	  @comment.user = current_user
 	  respond_to do |format|
+      
       if @comment.save
         format.html { redirect_to @product, notice: 'Review was created successfully.' }
         format.json { render :show, status: :created, location: @product }
@@ -12,7 +13,7 @@ class CommentsController < ApplicationController
         format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
-    end
+  end
   end
 
 	def destroy
@@ -28,5 +29,6 @@ private
   def comment_params
     params.require(:comment).permit(:user_id, :body, :rating)
   end
+
 end
 
