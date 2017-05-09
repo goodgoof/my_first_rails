04907ec0f98a7e3cfacb9b7743e720baf9d2ca12
goodgoof <anjalidsharma8@gmail.com>
2017-05-09 12:09:22 -0400

    fixed test in 6.2

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,8 +2,8 @@ require "rails_helper"
 
 describe Product do 
 	before do
-		@product = Product.create!(name:"race bike")
-		@user = User.create!(name:"John Smith", email:"test@test.com", password: "123456")
+		@product = Product.create!(name:"race bike", description:"24", price: "780.0", color: "red")
+		@user = User.create!(first_name:"John", last_name:"Smith", email:"test@test.com", password: "123456")
 		@product.comments.create!(rating: 1, user: @user, body: "Awful bike!")
 		@product.comments.create!(rating: 3, user: @user, body: "ok bike!")
 		@product.comments.create!(rating: 5, user: @user, body: "beautiful bike!")
