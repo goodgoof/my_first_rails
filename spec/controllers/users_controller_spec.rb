3fc70c78370b61b0f3ace30bfd542ef741c7fa97
goodgoof <anjalidsharma8@gmail.com>
2017-05-11 16:11:03 -0400

    fixed the functionale test for 6.3

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,44 +1,40 @@
 require "rails_helper"
 
-describe UserController, :type => :controller do 
+describe UsersController, :type => :controller do 
 	let (:user) {User.create!(email: 'peter@example.com', password: '1234567890')}
   	let (:different_user) {User.create!(email: 'quincy@medium.com', password: '0987654321')}
     
-    describe 'GET #show' do
+    	describe 'GET users#show' do
 
-      context 'User is logged in' do
-      	before do
-      	  sign_in user
+      		context 'User is logged in' do
+      			before do
+      	  			sign_in user
+			end
 
-      end
+        		it 'loads correct user details' do
+        			get :show, params: {id: user.id}
+        			expect(response).to have_http_status(200)
+        			expect(assigns(:user)).to eq user
+        	end
+			end
 
-        it 'loads correct user details' do
-        	get :show, params: {id: user.id}
-        	expect(response).to have_http_status(200)
-        	expect(assigns(:user)).to eq user
-        	
-        end
-
-
-      context 'No user is logged in' do
-       	it 'redirects to login' do
-	      get :show, params: {id: user.id}
-	      expect(response).to redirect_to(root_path)
-      end
-   end
+			context 'No user is logged in' do
+       			it 'redirects to login' do
+	      			get :show, params: {id: user.id}
+	      			expect(response).to redirect_to(new_user_session_path)
+			end
+			end
 
 
-   	   context "user tries to GET #show from different user" do
-   	   	before do
-   	   		sign_in different_user
-       end
+   	   		context "user tries to GET #show from different user" do
+   	   			before do
+   	   			sign_in different_user
+       			end
 
-       it 'is not allowed to view user #show' do
-        get :show, params: {id: user.id}
-        expect(response).to have_http_status(302)
-      end
-  end
-end
-	
-	
+       			it 'is not allowed to view user #show' do
+        			get :show, params: {id: user.id}
+        			expect(response).to have_http_status(302)
+			end
+  		end
+	end	
 end
