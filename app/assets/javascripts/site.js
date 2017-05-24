971be4eb6cf9fca78d633f7fd7437414b5152e62
goodgoof <anjalidsharma8@gmail.com>
2017-05-24 12:29:26 -0400

    6.9-fixed the bug in elevateZoom

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,13 +1,13 @@
 $(document).on('turbolinks:load', function(){
     $(".alert, .notice").delay(4000).fadeOut(1000);
-    $(".rating").raty({ path: "/assets", scoreName: "comment[rating]" });
+    $(".rating").raty({ path: "/assets", scoreName: "comment[rating]"});
     $(".rated").raty({ path: "/assets", 
     	readOnly: true,
     	score: function() {
       		return $(this).attr("data-score");
+        }
+    });
     $(".img-zoom").elevateZoom();
-    }
-  });
 });
     
     
