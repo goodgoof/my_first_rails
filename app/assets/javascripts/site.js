a3985836482113c0d565a999bdcbc171a8703094
goodgoof <anjalidsharma8@gmail.com>
2017-05-30 08:23:08 -0400

    6.10 jQuery

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,13 +1,23 @@
-$(document).on('turbolinks:load', function(){
-    $(".alert, .notice").delay(4000).fadeOut(1000);
-    $(".rating").raty({ path: "/assets", scoreName: "comment[rating]"});
-    $(".rated").raty({ path: "/assets", 
-    	readOnly: true,
-    	score: function() {
-      		return $(this).attr("data-score");
+var refreshRating = function() {
+    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+    $('.rated').raty({ path: '/assets',
+        readOnly: true,
+        score: function() {
+            return $(this).attr('data-score');
         }
     });
+};
+
+
+$(document).on('turbolinks:load ajaxSuccess', function(){
+    $(".alert, .notice").delay(4000).fadeOut(1000);
+
+    /* raty plugin */
+    refreshRating();
+    
+    /* elevate zoom plugin*/
     $(".img-zoom").elevateZoom();
+
 });
     
     
