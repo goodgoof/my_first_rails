356d5b79afc80ab78c88965ac88e405fd045c448
goodgoof <anjalidsharma8@gmail.com>
2017-05-31 13:00:45 -0400

    6.11 fixing caching

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,6 +52,14 @@ Rails.application.configure do
   config.log_tags = [ :request_id ]
 
   # Use a different cache store in production.
+  require 'dalli'
+  cache = Dalli::Client.new((ENV["MEMCACHIER_SERVERS"] || "").split(","),
+                    {:username => ENV["MEMCACHIER_USERNAME"],
+                     :password => ENV["MEMCACHIER_PASSWORD"],
+                     :failover => true,
+                     :socket_timeout => 1.5,
+                     :socket_failure_delay => 0.2
+                    })
   # config.cache_store = :mem_cache_store
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
